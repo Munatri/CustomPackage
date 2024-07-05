30c20295e0121df79195ba25031d3d4d5b0c58c2
Munatri <97083805+Munatri@users.noreply.github.com>
2024-07-05 17:37:13 +0530

    Add files via upload

---
+++ code/source/CustomPackage/javascv.java
@@ -0,0 +1,47 @@
+package CustomPackage;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import java.util.Random;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class javascv
+
+{
+	// ---( internal utility methods )---
+
+	final static javascv _instance = new javascv();
+
+	static javascv _newInstance() { return new javascv(); }
+
+	static javascv _cast(Object o) { return (javascv)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void generateUUID (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(generateUUID)>> ---
+		// @sigtype java 3.5
+		Random rand = new Random();
+		 int randomNum = rand.nextInt(10000);
+		 String outRandomNum=""+randomNum;
+		 
+		// pipeline
+		 IDataCursor pipelineCursor = pipeline.getCursor();
+		 IDataUtil.put( pipelineCursor, "outRandomNum", outRandomNum);
+		 pipelineCursor.destroy();
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
